40b5702c3fc81341a0994b3251ea70a6f816100f
Sam Tobin <smtobin@wisc.edu>
2026-08-06 22:19:35 -0400

    Initial implementation of rigid body gradients in triangle-rigid collision constraint

--- energy/TriangleRigidCollisionEnergySolver.hpp
+++ energy/TriangleRigidCollisionEnergySolver.hpp
@@ -93,8 +93,24 @@ struct TriangleRigidCollisionConstraintSolver
         if constexpr (DOF == 6)
         {
             /** TODO: (07/21/26) gradient w.r.t oriented particle */
-            C_grad_n = C_grad_t = C_grad_b = Vec6r::Zero();
+            Mat3r R = rb_rot.toRotationMatrix();
+            Mat3r skew_cp = Math::Skew3(cp_rb_local);
+            Mat3r R_rloc = R * skew_cp;
+            // gradients
+            C_grad_n.template block<3,1>(0,0) = n;
+            C_grad_n.template block<3,1>(3,0) = -n.transpose() * R_rloc;
+            C_grad_t.template block<3,1>(0,0) = t;
+            C_grad_t.template block<3,1>(3,0) = -t.transpose() * R_rloc;
+            C_grad_b.template block<3,1>(0,0) = b;
+            C_grad_b.template block<3,1>(3,0) = -b.transpose() * R_rloc;
+
+            // Hessians
+            // grad = (Skew(cp_rb_local) * R^T * n)^T
+            // ==> (Skew(cp_rb_local) * -R^T * skew(n) * -R)^T = R^T * skew(n) * R * skew(cp_rb_local)
             C_hess_n = C_hess_t = C_hess_b = Mat6r::Zero();
+            C_hess_n.template block<3,3>(3,3) = R.transpose() * Math::Skew3(n) * R * skew_cp;
+            C_hess_t.template block<3,3>(3,3) = R.transpose() * Math::Skew3(t) * R * skew_cp;
+            C_hess_b.template block<3,3>(3,3) = R.transpose() * Math::Skew3(b) * R * skew_cp;
         }
         else
         {
